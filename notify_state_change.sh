10abc767d614e0ed7e50c146e6c4bf9616917a4f
paul-kwangrag-son <paul.son@enterprisedb.com>
2025-09-05 22:27:29 +0900

    Update notify_state_change.sh

--- notify_state_change.sh
+++ notify_state_change.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # /etc/keepalived/notify_state_change.sh
-# 모든 VRRP 상태 변화를 로깅하는 범용 스크립트
+# General script to log all VRRP state changes
 
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 HOSTNAME=$(hostname)
@@ -8,7 +8,7 @@ CURRENT_STATE="$1" # MASTER, BACKUP, STOP
 
 logger "$TIMESTAMP INFO: [$HOSTNAME] Keepalived state changed to $CURRENT_STATE by notify_state_change.sh"
 
-# (옵션) 알림 추가
+# (Optional) Add notification
 # if [ "$CURRENT_STATE" == "MASTER" ]; then
 #   echo "Keepalived: MASTER on $HOSTNAME" | mail -s "HA Event" admin@example.com
 # elif [ "$CURRENT_STATE" == "BACKUP" ]; then
